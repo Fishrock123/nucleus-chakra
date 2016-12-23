a4b55c12163415aa78832b9867a82d57f982f776
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2016-12-23 11:02:19 -0500

    debug stuff, old chakracore update

--- build.rs
+++ build.rs
@@ -6,35 +6,45 @@
 
 use std::process::Command;
 use std::env;
-use std::path::Path;
+// use std::path::Path;
 
 fn main() {
     // gcc::compile_library("libduktape.a",
     //     &["deps/duktape-releases/src/duktape.c",
     //       "src/duk_rust_link.c"]);
 
-    let out_dir = env::var("OUT_DIR").unwrap();
+    // let out_dir = env::var("OUT_DIR").unwrap();
 
 // note that there are a number of downsides to this approach, the comments
 // below detail how to improve the portability of these commands.
     Command::new("bash").args(&["deps/ChakraCore/build.sh",
-                                "--static",
+                                // "--static",
                                 "--test-build",
                                 "--icu=/usr/local/opt/icu4c/include",
                                 "-j=8"])
                         .status().unwrap();
 
+
     let lib_path = "deps/ChakraCore/BuildLinux/Test/lib/";
 
+    println!("cargo:rustc-link-search=native={}{}", lib_path, "../pal/src/");
+    println!("cargo:rustc-link-lib=dylib=Chakra.Pal");
 
-    println!("cargo:rustc-link-search=native={}{}", lib_path,
-             "../pal/src/libChakra.Pal.a");
+    println!("cargo:rustc-link-search=native={}{}", lib_path, "Common/Core/");
+    println!("cargo:rustc-link-lib=dylib=Chakra.Common.Core");
 
-    println!("cargo:rustc-link-search=native={}{}", lib_path,
-             "Common/Core/libChakra.Common.Core.a");
+    println!("cargo:rustc-link-search=native={}{}", lib_path, "Jsrt/");
+    println!("cargo:rustc-link-lib=dylib=Chakra.Jsrt");
 
-    println!("cargo:rustc-link-search=native={}{}", lib_path,
-             "Jsrt/libChakra.Jsrt.a");
-             
-    println!("cargo:rustc-link-lib=static=ChakraCore");
+
+    let icu4c_path = "/usr/local/opt/icu4c/lib/";
+
+    println!("cargo:rustc-link-search=native={}", icu4c_path);
+    println!("cargo:rustc-link-lib=dylib=icudata");
+    println!("cargo:rustc-link-lib=dylib=icuuc");
+    println!("cargo:rustc-link-lib=dylib=icui18n");
+
+
+    println!("cargo:rustc-link-lib=framework=CoreFoundation");
+    println!("cargo:rustc-link-lib=framework=Security");
 }
